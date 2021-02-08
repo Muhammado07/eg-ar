030fbfac23f7a6b673acb1fb7fae119f7c5bc5ad
mohamedaly <mamohamedali691@gmail.com>
2021-02-08 17:03:25 +0200

    .

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const navbar = document.querySelector('.navbar')
-const logo = document.querySelector('.navbar .logo img')
-const caret = document.querySelectorAll('.navbar .subnavbtn img')
+const navbar = document.querySelector(".navbar")
+const logo = document.querySelector(".navbar .logo img")
+const caret = document.querySelectorAll(".navbar .subnavbtn img")
+
+const popBtn = document.querySelector(".pop-up")
+
+
 
 
 window.onscroll = function () {
@@ -8,31 +12,46 @@ window.onscroll = function () {
     // pageYOffset or scrollY
 
     if (window.pageYOffset > 80) {
-        navbar.classList.add('scrolled')
+        navbar.classList.add("scrolled")
         logo.setAttribute("src", "img/logo.png")
         caret.forEach(function (carets) {
             carets.setAttribute("src", "img/icon-caret.svg")
         })
 
+
     } else {
-        navbar.classList.remove('scrolled')
+        navbar.classList.remove("scrolled")
         logo.setAttribute("src", "img/logo-white.png")
         caret.forEach(function (carets) {
             carets.setAttribute("src", "img/caret-white.svg")
         })
 
     }
-    if (navbar.classList.contains('no')) {
+
+
+    if (navbar.classList.contains("no")) {
         logo.setAttribute("src", "img/logo.png")
         caret.forEach(function (carets) {
             carets.setAttribute("src", "img/icon-caret.svg")
         })
     }
+
+    // Show Btn pop-up
+
+    if (window.pageYOffset > 300) {
+        popBtn.classList.add("show")
+
+    } else {
+        popBtn.classList.remove("show")
+    }
+
 }
+
+
 
 // Date Year
 
-const year = document.querySelector('.year')
+const year = document.querySelector(".year")
 
 year.innerHTML = new Date().getFullYear()
 
@@ -40,7 +59,7 @@ year.innerHTML = new Date().getFullYear()
 
 // Mobile Nav
 
-const navExpand = [].slice.call(document.querySelectorAll('.nav-expand'));
+const navExpand = [].slice.call(document.querySelectorAll(".nav-expand"));
 const backLink = `<li class="nav-item">
 	<a class="nav-link nav-back-link" href="javascript:;">
 		Back
@@ -48,19 +67,30 @@ const backLink = `<li class="nav-item">
 </li>`;
 
 navExpand.forEach(item => {
-    item.querySelector('.nav-expand-content').insertAdjacentHTML('afterbegin', backLink);
-    item.querySelector('.nav-link').addEventListener('click', () => item.classList.add('active'));
-    item.querySelector('.nav-back-link').addEventListener('click', () => item.classList.remove('active'));
+    item.querySelector(".nav-expand-content").insertAdjacentHTML("afterbegin", backLink);
+    item.querySelector(".nav-link").addEventListener("click", () => item.classList.add("active"));
+    item.querySelector(".nav-back-link").addEventListener("click", () => item.classList.remove("active"));
 });
 
 
 // not-so-important stuff starts here
 
-const ham = document.getElementById('ham');
-ham.addEventListener('click', function () {
-    document.body.classList.toggle('nav-is-toggled');
+const ham = document.getElementById("ham");
+ham.addEventListener("click", function () {
+    document.body.classList.toggle("nav-is-toggled");
 });
 
+
+// Show Contact pop-up
+const pop = document.querySelector(".pop-over")
+const cancel = document.querySelector(".cancel")
+
+popBtn.addEventListener("click", function () {
+    pop.classList.add("view")
+})
+cancel.addEventListener("click", function () {
+    pop.classList.remove("view")
+})
 
 
 
