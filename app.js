8353b2371cc36ec67d672d210343e44297c8fed5
mohamedaly <mamohamedali691@gmail.com>
2021-03-08 10:24:08 +0200

    .

--- app.js
+++ app.js
@@ -45,6 +45,23 @@ year.innerHTML = new Date().getFullYear()
 
 
 
+document.querySelector('.custom-select-wrapper').addEventListener('click', function () {
+    this.querySelector('.custom-select').classList.toggle('open');
+})
+
+for (const option of document.querySelectorAll(".custom-option")) {
+    option.addEventListener('click', function () {
+        if (!this.classList.contains('selected')) {
+            this.parentNode.querySelector('.custom-option.selected').classList.remove('selected');
+            this.classList.add('selected');
+            this.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = this.textContent;
+        }
+    })
+}
+
+
+
+
 // Mobile Nav
 
 const navExpand = [].slice.call(document.querySelectorAll(".nav-expand"));
